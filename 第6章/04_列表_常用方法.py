8de17d80f3d6685b6c0ac60cdbab25a1ca3c3696
Ang <woeihaw94@gmail.com>
2026-06-06 09:57:58 +0800

    add code

---
+++ 第6章/04_列表_常用方法.py
@@ -0,0 +1,24 @@
+fruits = ['香蕉','苹果','橙子','香蕉']
+result = fruits.index("香蕉")
+print(result)
+
+nums = [10,20,10,30,10,40]
+result = nums.count(10)
+print(result)
+
+nums = [23,11,32,30,17]
+nums.reverse()
+print(nums)
+
+nums = [23,11,32,30,17]
+nums.sort(reverse=True)
+print(nums)
+
+# nums = [23,11,32,30,17,"尚硅谷"]
+# nums.sort(reverse=True)
+# print(nums)
+
+msg_list = ["北京","尚硅谷","你好"]
+msg_list.sort()
+print(msg_list)
+print(ord("你"),ord("北"),ord("尚"))
---
+++ 第6章/05_列表_常用内置函数.py
@@ -0,0 +1,38 @@
+# nums = [23,11,32,30,17]
+# result = sorted(nums,reverse=True)
+# print(nums)
+# print(result)
+
+# nums = [23,11,32,30,17,"尚硅谷"]
+# sorted(nums)
+
+# msg_list = ["北京","尚硅谷","你好"]
+# result = sorted(msg_list)
+# print(msg_list)
+# print(result)
+
+# nums = [10,20,10,30,10,40]
+# result = len(nums)
+# print(result)
+
+# nums = [23,11,32,30,17]
+# result = max(nums)
+# print(nums)
+# print(result)
+
+# nums = [23,11,32,30,17,"尚硅谷"]
+# result = max(nums)
+# print(nums)
+# print(result)
+
+# msg_list = ["北京","尚硅谷","你好"]
+# result = max(msg_list)
+# print(msg_list)
+# print(result)
+
+# result = max(33,45,12,78,99)
+# print(result)
+
+nums = [23,11,32,30,17]
+result = min(nums)
+print(result)
